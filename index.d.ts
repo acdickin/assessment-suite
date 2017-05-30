472f7075253c81d3f55e6c594fa00c92fed0e51c
Jack Lightfoot <jack.lightfoot@tee2.org>
2017-05-30 14:49:42 -0700

    establishing peer dependencies

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,29 @@
 import {Component} from 'React';
+interface AssessmentInterface{
+  onSubmit?(error: any, data: any): void;
+  values?: any;
+}
 
-export default assessments;
-declare namespace assessments{
-  interface AssessmentInterface{
-    onSubmit?(error: any, data: any): void;
-    values?: any;
-  }
 
-  export class FriendShip extends Component<null, null> implements AssessmentInterface{
+export class FriendShip extends Component<null, null> implements AssessmentInterface{
 
-  }
+}
 
-  export class MaritalSatisfaction extends Component<null, null> implements AssessmentInterface{
-    
-  }
+export class MaritalSatisfaction extends Component<null, null> implements AssessmentInterface{
   
-  export class PostDeploymentSocialSupport extends Component<null, null> implements AssessmentInterface{
-    
-  }
+}
 
-  export class SocialSupport extends Component<null, null> implements AssessmentInterface{
-    
-  }
+export class PostDeploymentSocialSupport extends Component<null, null> implements AssessmentInterface{
+  
+}
 
+export class SocialSupport extends Component<null, null> implements AssessmentInterface{
+  
+}
+
+export default {
+  FriendShip,
+  MaritalSatisfaction,
+  PostDeploymentSocialSupport,
+  SocialSupport
 }
