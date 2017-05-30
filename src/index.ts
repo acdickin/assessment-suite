436061b99370c790ab8da699245f51f88aa8b2bd
Jack Lightfoot <jack.lightfoot@tee2.org>
2017-05-30 12:49:50 -0700

    new files

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import FriendShipAssessment from './containers/assessments/FriendShip';
+import MaritalSatisfaction from './containers/assessments/MaritalSatisfaction';
+import PostDeploymentSocialSupport from './containers/assessments/PostDeploymentSocialSupport';
+import SocialSupport from './containers/assessments/SocialSupport';
+
+
+
+export {
+  FriendShipAssessment,
+  MaritalSatisfaction,
+  PostDeploymentSocialSupport,
+  SocialSupport 
+}
---
+++ src/www/index.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+//import { AppContainer } from 'react-hot-loader';
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
+
+import {
+  FriendShipAssessment,
+  MaritalSatisfaction,
+  PostDeploymentSocialSupport,
+  SocialSupport 
+} from '../';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+function testOnSuccess(data){
+  console.log(data);
+  console.log('testOnSuccess');
+}
+/*
+            <h1>Friendship</h1>
+        
+            <FriendShipAssessment />
+          
+            <h1>Marital Satisfaction</h1>
+
+            <MaritalSatisfaction  />
+
+            <h1>Post Deployment Support</h1>
+
+            <PostDeploymentSocialSupport />
+
+            <h1>Social Support</h1>
+            
+            <SocialSupport />
+*/
+
+const render = () => {
+    ReactDOM.render(
+          <div style={{padding: 20}}>
+          
+            <h1>Marital Satisfaction</h1>
+
+            <MaritalSatisfaction onSuccess={testOnSuccess} />
+
+          </div>,
+        document.getElementById("spaApp")
+    );
+}
+
+
+render();
+// Hot Module Replacement API. Only used when running the dev server.
+/*
+if ((module as any).hot) {
+  (module as any).hot.accept('./Provider', () => {
+    render(Provider)
+  });
+}
+*/
+
+
+
+
